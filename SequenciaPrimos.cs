c2ca885b4d07221c7e3122f27b72d817596c6b9c
Gustavo Santos <100400811+stringGustavo@users.noreply.github.com>
2022-11-27 09:22:03 -0300

    Sequência de Números Primos.

---
+++ SequenciaPrimos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SequenciaPrimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sequência de números primos.\n");
+
+            long quantidadeTermos, validarPrimo;
+
+            do{
+                Console.Write("Digite a quantidade de termos desejados: ");
+                quantidadeTermos = Convert.ToInt64(Console.ReadLine());
+
+                if(quantidadeTermos < 0)
+                {
+                    Console.WriteLine("\nQuantidade inválida!\n");
+                }
+            } while(quantidadeTermos < 0);
+
+            Console.WriteLine("");
+            for(int i = 2; i <= quantidadeTermos+1; i++)
+            {
+                validarPrimo = 1;
+                for(int j = 2; j < i; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        validarPrimo = 0;
+                    }
+                }
+
+                if(validarPrimo == 1)
+                {
+                    Console.Write("{0} ",i);
+                }
+                else
+                {
+                    quantidadeTermos++;
+                }
+            }
+
+        }
+    }
+}
