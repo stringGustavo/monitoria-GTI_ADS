64161627e66063634763a5ace42790852e52a35a
Gustavo Santos <100400811+stringGustavo@users.noreply.github.com>
2022-11-27 09:21:26 -0300

    Identificar Número Primo (Simples).

---
+++ PrimoSimples.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NumeroPrimoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, contador = 0;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= numero; i++)
+            {
+                if(numero % i == 0)
+                {
+                    contador++;
+                }
+            }
+
+            if(contador == 2)
+            {
+                Console.Write("\n{0} É primo.",numero);
+            }
+            else{
+                Console.Write("\n{0} Não é primo.",numero);
+            }
+        }
+    }
+}
